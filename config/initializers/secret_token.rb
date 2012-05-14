f47e8f79c9ba2c42a3d709430df8ace6d1202b04
Nicholas Mercer <nicholasjohnmercer@gmail.com>
2012-05-14 08:47:47 -0400

    Adding homepage.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homepage::Application.config.secret_token = '8c0f4464188a2af6a6e0f1aa440dc8b695e3f55f3f015f5895ffbddb7ffbd5f4c213ba9c2c735194724d5dfbe2aada81790ad9b13d69eb4b5c34247a23bd030b'
